5911f3e61819352cd74c0ea6e424f09f2ce3cc3e
j.lee4 <j.lee4@28b5cs07.sjsd.lcl>
2024-02-12 11:28:19 -0600

    my name is tyler

--- src/testing/cs42sap/curriculum/units/APUnit01PrimitiveTypes.java
+++ src/testing/cs42sap/curriculum/units/APUnit01PrimitiveTypes.java
@@ -34,7 +34,7 @@ public class APUnit01PrimitiveTypes
     public APUnit01PrimitiveTypes() {
         Simulator.header("AP: Unit 1 (Primitive Types) started...");
         
-        
+        // Me when I make a change:
         
         ////////////////////////////////////////////////////////////////////////
         Simulator.comment("1.1 Why Programming? Why Java?");        
