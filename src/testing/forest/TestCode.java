268edf1c26e6817dce4c4ff6f3011b581161ee8f
Forest <foxxpinkerton@gmail.com>
2024-02-15 11:47:35 -0600

    Fix auto loader

--- src/testing/forest/TestCode.java
+++ src/testing/forest/TestCode.java
@@ -1,33 +1,49 @@
 package testing.forest;
 
-import testing.forest.questions.*;
-
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
 
-/*
- * TestCode.java - Running some code for testing purposes
- *
- * @Author Forest
- */
 public class TestCode {
+
     public TestCode() {
-        initializeQuestions();
+        initializeQuestions("testing.forest.questions");
     }
 
-    private void initializeQuestions() {
-        try {
-            Class<?>[] questionClasses = {
-                    Question1.class,
-                    Question2.class,
-                    Question3.class
-            };
-
-            for (Class<?> questionClass : questionClasses) {
+    private void initializeQuestions(String packageName) {
+        List<Class<?>> questionClasses = getClasses(packageName);
+        for (Class<?> questionClass : questionClasses) {
+            try {
                 Constructor<?> constructor = questionClass.getDeclaredConstructor();
                 constructor.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+        }
+    }
+
+    private List<Class<?>> getClasses(String packageName) {
+        List<Class<?>> classes = new ArrayList<>();
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            String path = packageName.replace('.', '/');
+            java.net.URL resource = classLoader.getResource(path);
+            java.nio.file.Path directory = java.nio.file.Paths.get(resource.toURI());
+            java.nio.file.Files.walk(directory)
+                    .filter(p -> p.toString().endsWith(".class"))
+                    .forEach(p -> {
+                        String className = packageName + '.' + p.getFileName().toString().replace(".class", "");
+                        try {
+                            Class<?> cls = Class.forName(className);
+                            classes.add(cls);
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    });
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return classes;
     }
+
 }
