a260ca93d325d0533f43bcd0b23055d8ba967cc6
zelzmiy <adambelski2@gmail.com>
2024-02-28 12:00:03 -0600

    added basic tester for questions :(  i wanted to do the annotation thing but i couldn't figure it out :(

---
+++ src/questions/Q12.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package questions;
+
+/**
+ *
+ * @author a.belski
+ */
+public class Q12
+{
+        public Q12()
+        {
+            
+        }
+}
